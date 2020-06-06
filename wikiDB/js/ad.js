61edcf67e029c8cef240322faa6a0e4368fe5a79
Oein <62917247+Oein@users.noreply.github.com>
2020-06-06 15:41:11 +0900

    update

--- wikiDB/js/ad.js
+++ wikiDB/js/ad.js
@@ -5,8 +5,8 @@ function makeAD(){
         ad.id = "nbox1_func";
         iad.name = "adBox";
         iad.src = "http://npian.com/nbox/nbox_pu_4.asp?adid=Oein0219";
-        // iad.height = "1px";
-        // iad.width = "1px";
+        iad.height = "1px";
+        iad.width = "1px";
         iad.frameborder = "0";
         iad.scrolling = "no";
         iad.allowtransparency = "true";
